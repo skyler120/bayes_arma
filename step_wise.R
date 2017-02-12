46e84d0d7a364810662565bb4db3ee779239d9ad
skyler120 <dtsair@gmail.com>
2017-02-12 15:39:24 -0500

    added comparison for stepwise

--- step_wise.R
+++ step_wise.R
@@ -53,3 +53,33 @@ for(i in 1:length(vs)){
 }
 saveRDS(results55,file='BARMA_stepwise_21') # change file name!!!
 
+################# Full BARMA #############################
+results55 <- vector("list", length(vs))
+for(i in 1:length(vs)){
+  pt <- proc.time()[3]
+  print(i)
+  ev = matrix(-Inf,maxp+1,maxq+1)
+  x = vs[[i]]$series
+  y = vs[[i]]$forc
+  #run our algorithm
+  prev_pdm = -Inf
+  best_params = NULL
+  for(p in 0:maxp){
+    for(q in 0:maxq){
+      initialize = rep(0,p+q+1)
+      bayes_ar_res = bayes_arima(x,y,p,q, initialize)
+      ev[p+1,q+1] = bayes_ar_res$pdm
+      if(ev[p+1, q+1]> prev_pdm){
+        prev_pdm = ev[p+1, q+1]
+        best_params = bayes_ar_res$transformed_params
+      }
+    }
+  }
+  m = which(ev == max(ev), arr.ind = TRUE)
+  bp = m[1,1] - 1
+  bq = m[1,2] - 1
+  lag_terms = get_coeffs(best_params, bp, bq)
+  results55[[i]] = c(proc.time()[3] - pt, bp, bq, fitted_acc(x,y,bp,bq,rp,rq), lag_terms)
+}
+saveRDS(results55,file='BARMA_normal_21') # change file name!!!
+
