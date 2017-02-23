7b1d52edac07670c28ebf663c969f8bb8fe7627e
skyler120 <dtsair@gmail.com>
2017-02-22 19:18:54 -0500

    fixed demo with stepwise and added auto.arima comparison

--- demo_bayes_arma.R
+++ demo_bayes_arma.R
@@ -11,7 +11,7 @@ setwd("~/bayes_arma") #set the working directory - default will be home director
 source("all_code_get_results.R")                                                       #
 #                                                                                      #
 #                                                                                      #                            
-s = arima.sim(n=125,list(ar=c(0.48, -0.8), ma=c(0.2), sd=1))
+s = arima.sim(n=125,list(ar=c(0.6448287, -0.3062772 ), ma=c(-0.6780619), sd=1))
 #set this variable with the univariate time series you want to fit                     #
 x =    s[1:100]                                                                        #
 #set this variable with the univariate time series you want to forecast                #
@@ -29,7 +29,7 @@ results_file_name = "demo_res"; #name of file (string) to save output           
 ########################################################################################
 
 
-#####################################Bayes ARMA Algorithm###############################
+##################################### Bayes ARMA Algorithm #############################
 mx = mean(x); x = x - mx; #makes series zero mean                                      
 ev = matrix(-Inf,maxp+1,maxq+1)
 prev_pdm = -Inf
@@ -52,6 +52,12 @@ lag_terms = get_coeffs(best_params, bp, bq)
 res_barma = list(mat = ev, orders = c(bp,bq), acc = rfitted_acc(x,y,bp,bq, mx), coeffs = lag_terms)
 saveRDS(res_barma, results_file_name)
 
+##################################### Stepwise Algorithm ################################
+
+mx = mean(x); x = x - mx; #makes series zero mean                                      
+ev = matrix(-Inf,maxp+1,maxq+1)
+prev_pdm = -Inf
+best_params = NULL
 #step 1 initialization
   p = 2; q = 2; ev[p+1,q+1] = bayes_arima(x,y,p,q,rep(0,p+q+1))$pdm;
   p = 0; q = 0; ev[p+1,q+1] = bayes_arima(x,y,p,q,rep(0,p+q+1))$pdm;
@@ -61,11 +67,7 @@ saveRDS(res_barma, results_file_name)
   curr = c(m[1,1]-1, m[1,2]-1)
   curr_val = ev[curr[1]+1, curr[2]+1]
   
-  p=starting_vec[starting]; q=starting_vec[starting]; 
-  ev[p+1,q+1] = bayes_arima(x,y,p,q,rep(0,p+q+1))$pdm;
-  curr = c(p,q)
-  curr_val = ev[p+1,q+1]
-  #step 2
+#step 2
   count = 0
   params_list = numeric(16)
   while(count < (length(params_list)/2)){
@@ -89,3 +91,7 @@ saveRDS(res_barma, results_file_name)
   }
   res_step_barma = list(mat = ev, orders = curr, acc = rfitted_acc(x,y,curr[1],curr[2], mx))
   saveRDS(res_step_barma, paste("step_", results_file_name, sep=""))
+  
+################################### auto.arima code #######################################  
+a = auto.arima(x, d=0, max.p=maxp, max.q = maxq, allowmean = F, allowdrift = F, approximation = F, ic="bic", stepwise = F, max.order=maxp + maxq, stationary = T, seasonal = F)
+  
